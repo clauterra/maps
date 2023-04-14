899ca9a1e0ac73ce8d10736e7728c6e02c8e7ccf
clauterra <38335060+clauterra@users.noreply.github.com>
2023-04-14 17:00:39 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,5 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1_7p8ppgM4lrFchxrVtrzCaS2zYPK3W9rSEI2rRDP5W8/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = ''AIzaSyCfZ7Hx3UW7klKj6d8SEYr8oT_TDxKFX48'Y';
+//AIzaSyAiFz2ONVsOayCpudIPJ48mTs94g3ROtxM
+var googleApiKey = 'AIzaSyAiFz2ONVsOayCpudIPJ48mTs94g3ROtxM';
